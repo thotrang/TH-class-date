0a0bfed6799928d41ed54976afbad5df8347626b
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-05-19 16:18:26 +0700

    xây dựng lớp date

---
+++ date.js
@@ -0,0 +1,22 @@
+class Date{
+    day;
+    month;
+    year;
+    constructor(day,month,year) {
+        this.day=day;
+        this.month=month;
+        this.year=year;
+    };
+    getDay(){
+        return this.day;
+    }
+    getMonth(){
+        return this.month;
+    }
+    getYear(){
+        return this.year;
+    }
+
+
+
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+let date1=new Date(23,12,2000);
+alert(`birtday ${date1.getDay()}/${date1.getMonth()}/${date1.getYear()}`)
+
+
